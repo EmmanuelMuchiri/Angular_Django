156f883abe17f80662d18746c113c8bf1cb46617
emuchiri <emmanuel.muchiri@outlook.com>
2019-11-05 16:04:15 +0300

    This is working

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -10,9 +10,7 @@ router.register(r'users', views.UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
     url(r'^', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
